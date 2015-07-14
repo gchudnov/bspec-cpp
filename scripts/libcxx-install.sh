61d250e363016c7f4d8e1574c642bb2273f232ef
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-07-14 22:20:36 +0300

    travis - updated config for clang

--- scripts/libcxx-install.sh
+++ scripts/libcxx-install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+WORK_DIR=$(pwd)
 
 svn --quiet co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
 svn --quiet co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
 
 # libc++abi
 mkdir libcxxabi/build
-(cd libcxxabi/build && cmake .. -DLIBCXXABI_LIBCXX_PATH="$DIR/libcxx" -DLIBCXXABI_LIBCXX_INCLUDES="$DIR/libcxx/include" -DLLVM_CONFIG=/usr/local/bin/llvm-config)
+(cd libcxxabi/build && cmake .. -DLIBCXXABI_LIBCXX_PATH="$WORK_DIR/libcxx" -DLLVM_CONFIG=/usr/local/bin/llvm-config)
 make -C libcxxabi/build -j2
 
 sudo cp -r libcxxabi/build/lib/libc++abi.so.1.0 /usr/lib/
@@ -20,7 +20,7 @@ sudo cp -r libcxxabi/include/* /usr/include/c++/v1/
 
 # libc++
 mkdir libcxx/build
-(cd libcxx/build && cmake .. -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_CXX_ABI_INCLUDE_PATHS="$DIR/libcxxabi/include")
+(cd libcxx/build && cmake .. -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_CXX_ABI_INCLUDE_PATHS="$WORK_DIR/libcxxabi/include")
 make -C libcxx/build cxx -j2
 sudo cp libcxx/build/lib/libc++.so.1.0 /usr/lib/
 sudo ln -sf /usr/lib/libc++.so.1.0 /usr/lib/libc++.so
