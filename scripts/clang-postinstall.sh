32f7ae9eaafb1a65ecd677cae9111cea94443991
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-07-14 21:22:59 +0300

    travis - added clang log

--- scripts/clang-postinstall.sh
+++ scripts/clang-postinstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 # http://askubuntu.com/questions/309786/llvm-and-clang-installation-on-ubuntu
 sudo ln -sf -T /usr/bin/llvm-config-3.7 /usr/local/bin/llvm-config
@@ -7,3 +7,7 @@ sudo ln -sf -T /usr/bin/llvm-config-3.7 /usr/local/bin/llvm-config
 # https://llvm.org/bugs/show_bug.cgi?id=23352
 sudo mkdir -p /usr/lib/llvm-3.7/share/llvm
 sudo ln -sf -T /usr/share/llvm-3.7/cmake /usr/lib/llvm-3.7/share/llvm/cmake
+
+ls -la /usr/local/bin/llvm-config
+ls -la /usr/lib/llvm-3.7/share/llvm/cmake
+ls -la /usr/share/llvm-3.7/cmake
--- scripts/libcxx-install.sh
+++ scripts/libcxx-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
