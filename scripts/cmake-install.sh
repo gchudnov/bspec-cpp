c15c6996b14d711578d78f5d3dd9e386eaaee4c7
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-11-30 23:32:36 +0300

    upgrade cmake script

--- scripts/cmake-install.sh
+++ scripts/cmake-install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-wget http://www.cmake.org/files/v3.2/cmake-3.2.3-Linux-x86_64.sh
-chmod a+x cmake-3.2.3-Linux-x86_64.sh
-sudo ./cmake-3.2.3-Linux-x86_64.sh --skip-license --prefix=/usr/local
+SH_FILE='cmake-3.4.0-Linux-x86_64.sh'
+
+wget http://www.cmake.org/files/v3.4/${SH_FILE}
+chmod a+x ${SH_FILE}
+sudo ./${SH_FILE} --skip-license --prefix=/usr/local
 export PATH=/usr/local/bin:$PATH
 
 cmake --version
